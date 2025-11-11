187d0471fc706f0da68ce9132cee49ddc408ad57
nvnhat04 <nvnhat04324@gmail.com>
2025-11-11 21:11:34 +0700

    feat(app): add navigation and MainActivity

--- app/src/main/java/com/example/smartaq/ui/navigation/NaviGraph.kt
+++ app/src/main/java/com/example/smartaq/ui/navigation/NaviGraph.kt
@@ -4,11 +4,15 @@ import androidx.compose.runtime.Composable
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.compose.NavHost
 import androidx.navigation.NavHostController
+import androidx.navigation.NavType
 import androidx.navigation.compose.composable
 import androidx.navigation.navArgument
 import com.example.smartaq.ui.screens.AccountScreen
+import com.example.smartaq.ui.screens.AdvisorScreen
 import com.example.smartaq.ui.screens.HomeScreen
+import com.example.smartaq.ui.screens.MapsScreen
 import com.example.smartaq.ui.screens.NotificationScreen
+import com.example.smartaq.ui.screens.SearchScreen
 
 
 @Composable
@@ -42,18 +46,47 @@ fun NaviGraph(navController: NavHostController) {
                 navController = navController
             )
         }
-
+        composable("maps") {
+            MapsScreen(
+//                viewModel = viewModel(),
+                navController = navController
+            )
+        }
+        composable("search") {
+            SearchScreen(
+//                viewModel = viewModel(),
+                navController = navController
+            )
+        }
         composable("profile") {
             AccountScreen(
                 navController = navController,
 //                viewModel = viewModel()
             )
         }
-        composable("notification") {
+        composable("notifications") {
             NotificationScreen(
                 navController = navController,
 //                viewModel = viewModel()
             )
+        }
+//        composable("advisor") {
+//            AdvisorScreen(
+//                navController = navController,
+//                viewModel = viewModel(),
+//
+//            )
+//        }
+        composable(
+            route = "advisor?lat={lat}&lon={lon}",
+            arguments = listOf(
+                navArgument("lat") { type = NavType.FloatType; defaultValue = 21.0f },
+                navArgument("lon") { type = NavType.FloatType; defaultValue = 105.0f }
+            )
+        ) { backStackEntry ->
+            val lat = backStackEntry.arguments?.getFloat("lat")?.toDouble() ?: 21.0
+            val lon = backStackEntry.arguments?.getFloat("lon")?.toDouble() ?: 105.0
+            AdvisorScreen(viewModel = viewModel(), navController = navController, lat = lat, lon = lon)
         }
 
     }
